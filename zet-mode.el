a62aa75b1afac3d6a0cde3f8865f5803f52568a8
Turth <turth@users.noreply.github.com>
2024-09-29 11:51:37 -0600

    Pass flycheck

--- zet-mode.el
+++ zet-mode.el
@@ -1,5 +1,15 @@
+;;; package --- Summary
+;;; Commentary:
+;;; A mode for managing your Zettelkasten in Emacs.
+;;; Code:
+
+(require 'org-roam)
+
+(setq zet--org-link-id-regex "^[0-9a-f]\\{8\\}-[0-9a-f]\\{4\\}-[0-9a-f]\\{4\\}-[0-9a-f]\\{4\\}-[0-9a-f]\\{12\\}$")
+(setq zet--org-file-id-regex "^:ID: *\\([a-z0-9-]*\\)")
+
 (defun zet--generate-random-string (length)
-  "Generate a random string of a given LENGTH."
+  "Generate a random string lowercase characters of a given LENGTH."
   (let ((charset "abcdefghijklmnopqrstuvwxyz")
         (result ""))
     (dotimes (_ length result)
@@ -11,13 +21,15 @@
   (let ((title (concat "zet-" (zet--generate-random-string 6))))
     (org-roam-capture- :node (org-roam-node-create :title title))))
 
-(defun zet-org-roam-capture-random-bib (author)
-  "Capture a new org roam bibliographic note for the zettelkasten."
+(defun zet-org-roam-capture-random-bib (author-year)
+  "Capture a new bibliographic note for the zettelkasten.
+AUTHOR-YEAR is a string of the form <name>-<year>, for example, pirsig-74."
   (interactive "sEnter author:")
-  (let ((title (concat "bib-" author "-" (zet--generate-random-string 6))))
+  (let ((title (concat "bib-" author-year "-" (zet--generate-random-string 6))))
     (org-roam-capture- :node (org-roam-node-create :title title))))
 
 (defun zet--org-roam-capture-continuation-note (parent-file)
+  "Capture a new note and insert a link to PARENT-FILE."
   (let ((title (concat "zet_" (zet--generate-random-string 6))))
     (org-roam-capture-
      :node (org-roam-node-create :title title)
@@ -27,6 +39,7 @@
         :unnarrowed t)))))
 
 (defun zet--org-roam-capture-continuation-bib (parent-file)
+  "Capture a new bibliographic note and insert a link to PARENT-FILE."
   (let ((title (concat "bib_" (read-string "Enter author-year:") "-" (zet--generate-random-string 6))))
     (org-roam-capture-
      :node (org-roam-node-create :title title)
@@ -36,6 +49,7 @@
         :unnarrowed t)))))
 
 (defun zet--files-like (like-string)
+  "Return all files whose names names match the SQL query defined by LIKE-STRING, with ? as an escape character."
   (zet--unique-strings
    (mapcar (lambda (result) (zet--strip-directory (car result)))
            (org-roam-db-query
@@ -58,6 +72,7 @@
 
 
 (defun zet--files-containing-string (files string)
+  "Return the sublist of FILES which contain STRING."
   (let (result)
     (dolist (file files)
       (if (zerop (call-process "grep" nil nil nil "-q" "-i" "--" string file))
@@ -65,16 +80,19 @@
     (nreverse result)))
 
 (defun zet--files-containing-strings (files strings)
+  "Return the sublist of FILES which contain all of the specified STRINGS."
   (let ((result (zet--all-files-with-directory)))
     (dolist (string strings)
       (setq result (zet--files-containing-string result string)))
     result))
 
 (defun zet-selected-files-containing (string)
+  "Return the list of nodes which contain all of the space-separated strings passed by STRING."
   (interactive "sEnter target strings:")
   (zet--files-containing-strings (zet--all-files-with-directory) (split-string string " ")))
 
 (defun zet--unique-strings (list)
+  "Return LIST sorted alphabetically and with non-unique occurrences removed."
   (let ((sorted (sort list #'string-lessp))
         (result nil)
         (last ""))
@@ -85,31 +103,39 @@
     result))
 
 (defun zet--zet-files ()
+  "Return all files holding zettelkasten notes."
   (zet--files-like "%zet?_%.org"))
 
 (defun zet--bib-files ()
+  "Return all files containing bibliographic notes."
   (zet--files-like "%bib?_%.org"))
 
 (defun zet--all-files ()
+  "Return a list containing all zettelkasten notes and bibliographic notes."
   (append (zet--zet-files) (zet--bib-files)))
 
 (defun zet--all-files-with-directory ()
+  "Return a list containing all zettelkasten notes and bibliographic notes, with the parent directory attached."
   (mapcar (lambda (f) (concat (expand-file-name org-roam-directory) "/" f))
           (zet--all-files)))
 
 (defun zet--line-length ()
+  "Return the length of the current line."
   (- (line-end-position) (line-beginning-position)))
 
 (defun zet--remove-newlines (str)
+  "Return STR with newlines removed."
   (replace-regexp-in-string "\n" "" str))
 
 (defun zet--line-underneath-p ()
+  "Return non-nil iff there is a line underneath the current one."
   (save-excursion
     (forward-line 1)
     (end-of-line)
     (not (eobp))))
 
 (defun zet--truncate (str len)
+  "Truncate STR to at most LEN characters."
   (substring str
              0
              (if (> len (length str))
@@ -117,6 +143,7 @@
                len)))
 
 (defun zet--print-columns (columns)
+  "Print COLUMNS nicely in the current buffer."
   (setq buffer-read-only nil)
   (erase-buffer)
   (setq zet--column-object-to-file (make-hash-table :test #'equal))
@@ -138,6 +165,7 @@
       (setq n-col (+ n-col 1)))))
 
 (defun zet--text-object-at-point ()
+  "Get the contiguous text object under the point."
   (let ((start (point))
         (end (point))
         (original (point)))
@@ -153,6 +181,7 @@
     (zet--remove-newlines (buffer-substring-no-properties start end))))
 
 (defun zet-next-text-object ()
+  "Move forward to the next text object on the line."
   (interactive)
   (while (and (not (looking-at-p "[ \n\t]"))
               (not (eolp)))
@@ -162,6 +191,7 @@
     (goto-char (+ (point) 1))))
 
 (defun zet-prev-text-object ()
+  "Move backward to the previous text object on the line."
   (interactive)
   (while (and (not (looking-at-p "[ \n\t]"))
               (not (bolp)))
@@ -171,6 +201,7 @@
     (goto-char (- (point) 1))))
 
 (defun zet--files-linking-to (id)
+  "Return a list of nodes linking to ID."
   (mapcar (lambda (result) (zet--strip-directory (car result)))
           (org-roam-db-query
            [:select [file]
@@ -181,6 +212,7 @@
            id)))
 
 (defun zet--files-linked-from (id)
+  "Return a list of nodes which are linked to from the node with id ID."
   (mapcar (lambda (result) (zet--strip-directory (car result)))
           (org-roam-db-query
            [:select [file]
@@ -190,41 +222,63 @@
                     :where (= links:source $s1)]
            id)))
 
-(defun zet-purge ()
+(defun zet--dead-links ()
+  "Return a list of links with no target."
+  (let* ((query "SELECT source, dest, file
+               FROM links INNER JOIN nodes
+               ON links.source = nodes.id
+               AND dest NOT IN (SELECT id FROM nodes)")
+         (results (org-roam-db-query query))
+         (to-modify '()))
+    (dolist (result results)
+      (if (string-match-p zet--org-link-id-regex (cadr result))
+          (setq to-modify (cons result to-modify))))
+    (setq to-modify
+          (sort to-modify #'(lambda (a b) (string-lessp (caddr a) (caddr b)))))
+    to-modify))
+
+(defun zet-prune-dead-links ()
+  "Visit all files in the zettelkasten and remove their dead links, replacing the org link with its text representation."
   (interactive)
-  (let ((n-deleted 0)
-        (id (org-id-store-link))
-        (files (zet--files-linking-to (buffer-file-name))))
-    (if files
+  (let ((n-pruned 0)
+        (to-modify (zet--dead-links)))
+    (if to-modify
         (save-excursion
-          (dolist (file files)
-            (find-file file)
-            (goto-char (point-min))
-            (while (re-search-forward (org-id-link-regex id) nil t)
-              (replace-match "\\1")
-              (setq n-deleted (1+ n-deleted))))
-          (message (format "Deleted %s links" n-deleted))))))
+          (dolist (modify to-modify)
+            (if (y-or-n-p (format "Really prune %s in %s?" (cadr modify) (caddr modify)))
+                (progn
+                  (find-file (caddr modify))
+                  (goto-char (point-min))
+                  (while (re-search-forward (org-id-link-regex (cadr modify)) nil t)
+                    (replace-match "\\1")
+                    (message "Replaced lin")
+                    (setq n-pruned (1+ n-pruned))))))
+          (message (format "Pruned %s links" n-pruned)))
+      (message "No dead links, congratulations!"))))
 
 (defun zet--prepend-directory (file)
+  "Prepend the org-roam-directory to FILE if it is not yet an absolute path."
   (if (string-match-p "^/.*" file)
       file
     (concat (file-name-as-directory (expand-file-name org-roam-directory)) file)))
 
 (defun zet--strip-directory (file)
+  "Remove the directory component of FILE if it is an absolute path."
   (if (string-match-p "^/.*" file)
       (zet--last (string-split file "/"))
     file))
 
-(setq zet--org-id-regex "^:ID: *\\([a-z0-9-]*\\)")
 (defun zet--org-id (file)
+  "Get the org roam id of FILE."
   (with-temp-buffer
     (insert-file-contents (zet--prepend-directory file))
     (goto-char (point-min))
-    (if (re-search-forward zet--org-id-regex nil t)
+    (if (re-search-forward zet--org-file-id-regex nil t)
         (match-string 1)
       nil)))
 
 (defun zet--org-timestamp (file)
+  "Return the org timestamp of FILE if it exists, else nil."
   (let ((full-file (zet--prepend-directory file)))
     (if (file-exists-p full-file)
         (with-temp-buffer
@@ -236,6 +290,7 @@
       nil)))
 
 (defun zet--sorted-by-ts (files)
+  "Sort the org roam FILES by their timestamp, with nil timestamp files at the start."
   (let ((file-to-ts
          (mapcar
           (lambda (file) (cons file (zet--org-timestamp file)))
@@ -253,42 +308,53 @@
                                        (org-time-string-to-time tb))))))))))
 
 (defun zet--all-files-sorted-by-ts ()
+  "Return a list of all files in the zettelkasten sorted by their timestamp."
   (zet--sorted-by-ts (zet--all-files)))
 
 (defun zet--last (list)
+  "Return the car of the last element of LIST."
   (car (last list)))
 
 (defun zet--last-file ()
+  "Return the last file which was added to the zettelkasten.
+Useful for identifying a node to continue your train of thought from."
   (zet--last (zet--all-files-sorted-by-ts)))
 
-(setq zet--selected-node nil)
 (defun zet--current-node ()
+  "Return the 'current' node of the zettelkasten, which is either the last added node or the selected node."
   (let ((current (or zet--selected-node (zet--last-file))))
     (if (null zet--selected-node)
         (setq zet--selected-node current))
     current))
 
 (defun zet--set-selected-node ()
+  "Set the selected node to the file currently under the point."
   (setq zet--selected-node (zet--strip-directory (zet--file-at-point)))
   (message (format "Selected node %s" zet--selected-node)))
 
 (defun zet-randomize-selected-node ()
+  "Randomize the current selected node.
+Useful for taking a random walk through your slipbox."
   (interactive)
   (setq zet--selected-node (choose (zet--all-files))))
 
 (defun zet--files-linking-to-current ()
+  "Return a list of files linking to the curren file."
   (zet--files-linking-to (zet--org-id (zet--current-node))))
 
 (defun zet--files-linked-from-current ()
+  "Return a list of files linked to from the current file."
   (zet--files-linked-from (zet--org-id (zet--current-node))))
 
 (defun zet--stringmul (scalar string)
+  "Return the string formed by concatenating STRING to itself SCALAR times."
   (let ((result ""))
     (dotimes (_ scalar)
       (setq result (concat result string)))
     result))
 
 (defun zet-print-link-map ()
+  "Print three columns. The files which link to the current file, the current file, and files linked to from the current file."
   (interactive)
   (zet--print-columns
    (list
@@ -299,18 +365,21 @@
   (zet-next-text-object))
 
 (defun zet--print-all-files ()
+  "Print all files in the zettelkasten, sorted by timestamp."
   (interactive)
   (zet--print-columns
    (list (zet--all-files-sorted-by-ts)))
   (goto-char (point-min)))
 
 (defun zet-print-isolated-files ()
+  "Print the files in the zettelkasten which are not linked to and link to no other files."
   (interactive)
   (zet--print-columns
-   (list (zet--org-roam-files-with-no-links)))
+   (list (zet--sorted-by-ts (zet--org-roam-files-with-no-links))))
   (goto-char (point-min)))
 
 (defun zet-print-files-containing-strings (strings)
+  "Print the files which contain all space-separated strings passed in STRINGS."
   (interactive "sEnter strings:")
   (zet--print-columns
    (list (zet--files-containing-strings (zet--all-files-with-directory) (split-string strings " "))))
@@ -321,10 +390,12 @@
   (zet--initialize))
 
 (defun zet--file-at-point ()
+  "Return the fully qualified file name of the file under the point."
   (zet--prepend-directory
    (gethash (zet--text-object-at-point) zet--column-object-to-file)))
 
 (defun zet-view-file ()
+  "Open a file for viewing."
   (interactive)
   (let ((file (zet--file-at-point)))
     (if file
@@ -334,20 +405,57 @@
           (other-window -1)))))
 
 (defun zet--mark-from ()
+  "Mark the file under the point as the file to link from."
   (interactive)
   (setq zet--from-file (zet--file-at-point))
   (message (format "From link is now %s" zet--from-file)))
 
 (defun zet--mark-to ()
+  "Mark the file under the point as the file to link to."
   (interactive)
   (setq zet--to-file (zet--file-at-point))
   (message (format "To link is now %s" zet--to-file)))
 
+(defun zet-mark-for-deletion ()
+  "Mark the file under the point for deletion."
+  (interactive)
+  (push (zet--file-at-point) zet--to-trash)
+  (message (format "Marked %s for deletion" (zet--file-at-point))))
+
+(defun zet-move-file-to-trash (file)
+  "Move FILE to ~/.trash."
+  (interactive "fMove file to trash: ")
+  (let ((trash-dir (expand-file-name "~/.trash"))
+        (filename (file-name-nondirectory file)))
+    (unless (file-directory-p trash-dir)
+      (make-directory trash-dir t))
+    (rename-file file (expand-file-name filename trash-dir))
+    (message "Moved %s to %s" file trash-dir)))
+
+(defun zet-trash-files ()
+  "Move all files marked for deletion to the trash."
+  (interactive)
+  (setq zet--to-trash
+        (zet--filter (lambda (f) (file-exists-p f)) zet--to-trash))
+  (dolist (file zet--to-trash)
+    (message (format "Trashing %s" file))
+    (zet-move-file-to-trash file)))
+
+(defun zet--filter (filter list)
+  "Return the sublist of LIST of elements which pass the FILTER, a function of a single argument."
+  (let ((result '()))
+    (dolist (thing list)
+      (if (funcall filter thing)
+          (push thing result)))
+    result))
+
 (defun zet--link-text (file)
+  "Return the org link text for linking to FILE."
   (let ((id (zet--org-id file)))
     (format "[[id:%s][%s]]" id (zet--strip-directory file))))
 
 (defun zet-link ()
+  "Insert a link in the 'from' file to the 'to' file."
   (interactive)
   (if (null zet--from-file)
       (user-error "Must set from file"))
@@ -366,6 +474,7 @@
         (user-error "No timestamp to link after")))))
 
 (defun zet--replace (text replacement)
+  "Replace the next instance of TEXT with REPLACEMENT."
   (save-excursion
     (if (search-forward text nil t)
         (progn
@@ -377,10 +486,14 @@
       (user-error "No occurrence of" text))))
 
 (defun zet--initialize ()
+  "Initialize zet mode, setting state variables to their initial values."
+  (setq zet--selected-node nil)
   (setq zet--from-file nil)
-  (setq zet--to-file nil))
+  (setq zet--to-file nil)
+  (setq zet--to-trash nil))
 
 (defun zet-main ()
+  "Entrypoint for zet mode. Call the initialization function and open an initial view into the zettelkasten."
   (interactive)
   (let ((buffer (get-buffer-create "*Zettelkasten*")))
     (switch-to-buffer buffer)
@@ -391,6 +504,7 @@
     (zet-mode)))
 
 (defun zet--current-ids ()
+  "Return a list of the org ids of each file in the zettelkasten."
   (mapcar #'car
           (org-roam-db-query
            [:select [id]
@@ -400,10 +514,10 @@
                     :escape $r1]
            "?")))
 
-
-(global-set-key (kbd "C-c z") 'zet-main)
-
 (defmacro zet--local (&rest body)
+  "Macro for defining functions more easily using general.el. Function body is the list of forms in BODY."
   `(lambda () (interactive) ,@body))
 
+(global-set-key (kbd "C-c z") 'zet-main)
 (provide 'zet-mode)
+;;; zet-mode.el ends here
